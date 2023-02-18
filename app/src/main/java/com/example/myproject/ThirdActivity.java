33d5d868b6306ccdf42b3be10b493ca05c89a055
JM-creater <garadojosephmartin98@gmail.com>
2023-02-19 06:32:46 +0800

    test Project

--- app/src/main/java/com/example/myproject/ThirdActivity.java
+++ app/src/main/java/com/example/myproject/ThirdActivity.java
@@ -2,26 +2,28 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
 
-        Button button = (Button) findViewById(R.id.done2);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button button = (Button) findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                Intent intent = new Intent(ThirdActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
 
     }
 }
